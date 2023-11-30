8c99f40d95dae182c57c26ef0cb69a03e2ec93ed
Miguel Echenique Palacios <miguel.echenique@audiense.com>
2023-11-30 11:57:08 +0100

    remove builder from course-duration

--- course-duration/test/course.test.ts
+++ course-duration/test/course.test.ts
@@ -2,88 +2,76 @@ import {Course} from "../src/course";
 import {Timer} from '../src/timer';
 import {College} from "../src/college";
 
-
-class BuilderCourse {
-    private duration: number=0;
-    private college: string|undefined='123';
-    private startTime: number = 15;
-    private name: string= 'test';
-    constructor() {
-    }
-
-    withDuration(duration:number){
-        this.duration = duration;
-        return this;
-    }
-
-    withCollege(college:string|undefined){
-        this.college=college;
-        return this;
-    }
-
-    withName(name:string){
-        this.name=name;
-        return this;
-    }
-
-    build(){
-        const testTimer = new Timer();
-        const fakeGetTime = jest.fn(() => this.startTime)
-        testTimer.getTime = fakeGetTime;
-
-        const testCollege = new College();
-        const fakeGetEnv= jest.fn((): string | undefined => '');
-        testCollege.getName = fakeGetEnv;
-        fakeGetEnv.mockReturnValue(this.college);
-
-        const course = new Course(this.name, testTimer,testCollege);
-
-        course.start();
-        fakeGetTime.mockReturnValue(this.duration+this.startTime);
-        course.end();
-
-        return course;
-    }
+const aTimer = (startTime: number) =>{
+    const testTimer = new Timer();
+    const fakeGetTime = jest.fn(() => startTime)
+    testTimer.getTime = fakeGetTime;
+    return testTimer as Timer & {getTime: typeof fakeGetTime};
 }
-const courseName = 'macramé';
+
+const aCollege = (college?: string) =>{
+    const testCollege = new College();
+    const fakeCollege= jest.fn((): string | undefined => '');
+    testCollege.getName = fakeCollege;
+    fakeCollege.mockReturnValue(college);
+    return testCollege as Timer & {getName: typeof fakeCollege};
+}
+
+const aCourse = (duration:number, college?:string,name:string='testName')=>{
+    const startTime = 15;
+    const testTimer = aTimer(startTime);
+    const testCollege = aCollege(college)
+
+    const course = new Course(name, testTimer, testCollege);
+
+    course.start();
+    testTimer.getTime.mockReturnValue(duration+startTime);
+    course.end();
+
+    return course;
+}
+
 
 
 describe('Course', () => {
 
-    it('identifies short courses', () => {
-        const course = new BuilderCourse()
-            .withDuration(10 * 60 * 60 * 1000 - 1)
-            .build();
+    const courseName = 'macramé';
+    const minLongCourseDuration = 10 * 60 * 60 * 1000;
 
+
+    it('identifies short courses', () => {
+
+        const course = aCourse(minLongCourseDuration - 1)
 
         expect(course.isShort())
             .toBe(true);
     });
 
     it('identifies long courses',()=>{
-        const longCourse= new BuilderCourse()
-            .withDuration(10 * 60 * 60 * 1000)
-            .build()
+        const longCourse= aCourse(minLongCourseDuration)
 
         expect(longCourse.isLong())
             .toBe(true)
     });
 
     it('knows the course title',()=>{
-        const course = new BuilderCourse()
-                .withCollege('testCollege')
-                .withName(courseName)
-                .build();
+        const course = aCourse(
+            0,
+            'testCollege',
+            courseName
+        )
 
         expect(course.getTitle())
             .toBe(`${courseName} course in testCollege college`);
     });
 
     it('knows the course title when college is undefined',()=>{
-        const course = new BuilderCourse()
-            .withCollege(undefined)
-            .withName(courseName)
-            .build();
+        const course = aCourse(
+            0,
+            undefined,
+            courseName
+        )
+
 
         expect(course.getTitle())
             .toBe(`${courseName} course in not found college`);
